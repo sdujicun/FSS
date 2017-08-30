fd9259873f9a22d9442da1b846d64a1d84cfea64
jicun <jicun@sdu.edu.cn>
2017-08-30 08:53:00 +0800

    create

---
+++ README.md
@@ -0,0 +1,5 @@
+# FSS
+
+The code for FSS.  
+All the experiments are in the package "FSS_experiments".   
+The results are in the file which name is "result".
---
+++ src/FSS_experiments/AccuracyTest.java
@@ -0,0 +1,67 @@
+package FSS_experiments;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+
+import utilities.ClassifierTools;
+import weka.classifiers.meta.timeseriesensembles.WeightedEnsemble;
+import weka.core.Instances;
+import weka.core.shapelet.QualityMeasures;
+import weka.filters.timeseries.shapelet_transforms.classValue.BinarisedClassValue;
+import weka.filters.timeseries.shapelet_transforms.subclass.ShapeletTransformWithSubclassSampleAndLFDP;
+import weka.filters.timeseries.shapelet_transforms.subsequenceDist.ImprovedOnlineSubSeqDistance;
+import development.DataSets;
+
+public class AccuracyTest {
+	public static void main(String[] args) throws Exception {
+		
+		String[] problems=DataSets.DSUsed;	
+		
+		System.out.println(problems.length);
+		for (int i = 0; i < problems.length; i++) {
+			System.out.print(problems[i] + "\t");
+			accuracyForFSS(problems[i]);
+			System.out.println();
+
+		}
+	}
+
+	public static void accuracyForFSS(String problem) throws Exception {
+
+		String filename = "accuracy.txt";
+		File f = new File("./" + File.separator + filename);
+		Writer out = null;
+		out = new FileWriter(f, true);
+		final String resampleLocation = DataSets.problemPath;
+		final String dataset = problem;
+		final String filePath = resampleLocation + File.separator + dataset + File.separator + dataset;
+
+		Instances test, train;
+		test = utilities.ClassifierTools.loadData(filePath + "_TEST");
+		train = utilities.ClassifierTools.loadData(filePath + "_TRAIN");
+		ShapeletTransformWithSubclassSampleAndLFDP transform = new ShapeletTransformWithSubclassSampleAndLFDP();
+		transform.setRoundRobin(true);
+
+		transform.setClassValue(new BinarisedClassValue());
+		transform.setSubSeqDistance(new ImprovedOnlineSubSeqDistance());
+		transform.useCandidatePruning();
+		transform.setNumberOfShapelets(train.numInstances() / 2);
+		transform.setQualityMeasure(QualityMeasures.ShapeletQualityChoice.INFORMATION_GAIN);
+
+		Instances tranTrain = transform.process(train);
+		Instances tranTest = transform.process(test);
+
+		double accuracy;
+		WeightedEnsemble we = new WeightedEnsemble();
+		we.buildClassifier(tranTrain);
+		accuracy = ClassifierTools.accuracy(tranTest, we);
+
+		
+		out.write(problem + "\t" + accuracy + "\t\r\n");
+		out.close();
+		System.out.print(accuracy + "\t");
+
+	}
+
+}
